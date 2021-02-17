421a4ba58aa3be434ab092dbc98824c1b98b8faa
davestroud <david@davidstroud.me>
2021-02-17 11:21:16 -0500

    add libraries

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import yfinance as yf
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import datetime
+import warnings
+warnings.filterwarnings('ignore')
 
 spy = yf.Ticker("SPY")
 
